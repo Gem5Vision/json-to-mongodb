c2b75477e96817ee7abc5b596a9b164cca7fffe6
kunpai <kunpai@ucdavis.edu>
2023-03-10 23:27:18 -0800

    Algo to extract tested examples

---
+++ tested.py
@@ -0,0 +1,55 @@
+import json
+import os
+
+base_url = "https://github.com/gem5/gem5/tree/develop"
+folder_path = 'gem5/configs/example'
+folder_path_2= 'gem5/tests/gem5'
+files = []
+for root, directories, filenames in os.walk(folder_path):
+    for filename in filenames:
+        files.append(filename)
+
+# Print the file names
+print(files)
+# Search for each file in folder_path_2
+for file in files:
+    for root, directories, filenames in os.walk(folder_path_2):
+        for filename in filenames:
+            if filename.endswith('.py'):
+                with open(os.path.join(root, filename), 'r') as f:
+                    if file in f.read():
+                        print(f'"{file}" found in {os.path.join(root, filename)}')
+
+# def search_folder(folder_path, id):
+#         matching_files = []
+#         for filename in os.listdir(folder_path):
+#             file_path = os.path.join(folder_path, filename)
+#             if os.path.isfile(file_path):
+#                 with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+#                     contents = f.read()
+#                     if id in contents:
+#                         matching_files.append(file_path)
+#             elif os.path.isdir(file_path):
+#                 matching_files.extend(search_folder(file_path, id))
+#         return matching_files
+# # load the JSON file
+# with open('resources.json', 'r+') as f:
+#     data = json.load(f)
+
+#     # ids = [obj['id'] for obj in data]
+#     for resource in data:
+#         print(resource)
+#         id=resource['id']
+#         # search for files in the folder tree that contain the 'id' value
+#         matching_files = search_folder(folder_path, id)
+
+#         # print the list of matching files for this 'id'
+#         print('Files containing id {}:'.format(id))
+#         matching_files = [file[5:] for file in matching_files]
+#         matching_files = [base_url + '/' + file for file in matching_files]
+#         resource['example_urls']=matching_files
+    
+#     # write the JSON file
+#     f.seek(0)
+#     f.write(json.dumps(data, indent=4))
+    
